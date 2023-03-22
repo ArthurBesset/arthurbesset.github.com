436941ebe7e83acd585019568a98cd3a650ca2d0
Besset Arthur <arthur@MacBook-Air-de-Besset.local>
2023-03-22 15:04:49 +0100

    start

---
+++ script.js
@@ -0,0 +1,11 @@
+//Menu Burger
+
+let link = document.getElementById('link')
+let burger = document.getElementById('burger')
+let ul = document.querySelector('ul')
+
+link.addEventListener('click', function(e) {
+  e.preventDefault()
+  burger.classList.toggle('open')
+  ul.classList.toggle('open')
+})
